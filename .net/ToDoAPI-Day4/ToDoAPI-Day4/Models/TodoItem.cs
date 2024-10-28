085fb75b63ea8cc9682057e9fa18011909a454d3
ManuvidyaS <manuvidyasmd@gmail.com>
2024-10-28 14:03:15 +0530

    .net 2 added

---
+++ .net/ToDoAPI-Day4/ToDoAPI-Day4/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoAPI_Day4.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
